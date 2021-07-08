6928379dee60fbf123676b57f9c87dfa622fce25
YDTRINLEY <ydtrinley@gmail.com>
2021-07-08 16:58:57 +0000

    Site updated: 2021-07-08 16:58:57

--- js/custom.js
+++ js/custom.js
@@ -65,50 +65,6 @@ $(function() {
 })
 
 
-/* 点击水波纹特效 */
-$(function() {
-	try {
-		$('body').ripples({
-			resolution: 512,
-			dropRadius: 20, //px
-			perturbance: 0.04,
-		});
-		$('main').ripples({
-			resolution: 128,
-			dropRadius: 10, //px
-			perturbance: 0.04,
-			interactive: false
-		});
-	}
-	catch (e) {
-		$('.error').show().text(e);
-	}
-
-	$('.js-ripples-disable').on('click', function() {
-		$('body, main').ripples('destroy');
-		$(this).hide();
-	});
-
-	$('.js-ripples-play').on('click', function() {
-		$('body, main').ripples('play');
-	});
-
-	$('.js-ripples-pause').on('click', function() {
-		$('body, main').ripples('pause');
-	});
-
-	// Automatic drops
-	setInterval(function() {
-		var $el = $('main');
-		var x = Math.random() * $el.outerWidth();
-		var y = Math.random() * $el.outerHeight();
-		var dropRadius = 20;
-		var strength = 0.04 + Math.random() * 0.04;
-
-		$el.ripples('drop', x, y, dropRadius, strength);
-	}, 400);
-});
-
 
 /* 手机侧边栏默认不展开 */
 var mobile_sidebar_menus = document.getElementById("sidebar-menus");
