d1c32afd76cc1362340bedcc5083a9e08dc5b620
YDTRINLEY <ydtrinley@gmail.com>
2021-09-19 06:55:25 +0000

    Site updated: 2021-09-19 06:55:25

--- js/fcircle.js
+++ js/fcircle.js
@@ -1,142 +1,170 @@
-var friend_link_circle = new Vue({
-    el: '#friend_link_circle',
-    data: {
-        datalist: [],
-        datalist_slice:[],
-        maxnumber:20,
-        addnumber:10,
-        display:true,
-        loadmore_display:false,
-        listlenth:0,
-        today_post:0,
-        last_update_time:'',
-        user_lenth:'',
-        error:0,
-        unique_live_link:0,
-        opentype:'_blank'  //'_blank'打开新标签,'_self'本窗口打开
-    },
-    methods:{
-        unique (arr) {
-            return Array.from(new Set(arr))
-        },
-        formatDate(strDate) {
-            try{
-                let date = new Date(Date.parse(strDate.replace(/-/g, "/")));
-                let gettimeoffset = 0
-                if (new Date().getTimezoneOffset()){
-                    gettimeoffset = new Date().getTimezoneOffset();
-                }
-                else{
-                    gettimeoffset = 8;
-                }
-                let timeoffset = gettimeoffset * 60 * 1000;
-                let len = date.getTime();
-                let date2 = new Date(len - timeoffset);
-                let sec = date2.getSeconds().toString();
-                let min =  date2.getMinutes().toString();
-                if (sec.length === 1) {
-                    sec = "0" + sec;
-                }
-                if (min.length === 1) {
-                    min = "0" + min;
-                }
-                return date2.getFullYear().toString() + "/" + (date2.getMonth() + 1).toString() + "/" + date2.getDate().toString() + " " + date2.getHours().toString() + ":" + min + ":" + sec
-            }catch(e){return ""}
-        },
-        timezoon(){
-            let time = this.datalist_slice[0][1][0][5];
-            return this.formatDate(time)
-        },
-        todaypost(){
-            let date= new Date();
-            let year = date.getFullYear();
-            let month =(date.getMonth() + 1).toString();
-            let day = (date.getDate()).toString();
-            if (month.length === 1) {
-                month = "0" + month;
-            }
-            if (day.length === 1) {
-                day = "0" + day;
-            }
-            return  year + "-" + month + "-" + day
-        },
-        addmaxnumber(){
-            this.maxnumber = this.maxnumber + this.addnumber;
-            if (this.maxnumber >= this.listlenth){
-                this.loadmore_display=false;
-            }
-        },
-        slice(data){
-            let monthlist=[];
-            let datalist=[];
-            let data_slice = data;
-            for (let item in data_slice) {
-                data_slice[item].push(item);
-                if (data_slice[item][1].lenth !== 10) {
-                    let list = data_slice[item][1].split('-')
-                        if (list[1].length < 2){
-                            list[1]="0" + list[1]
-                        }
-                        if (list[2].length < 2){
-                            list[2]="0" + list[2]
-                        }
-                    data_slice[item][1] = list.join('-')
-                    }
-                let month=data_slice[item][1].slice(0,7);
-                if(monthlist.indexOf(month) !== -1){
-                    console.log(month);
-                    datalist[monthlist.length-1][1].push(data_slice[item]);
-                }
-                else{
-                    monthlist.push(month);
-                    datalist.push([month,[data_slice[item]]]);
-                }
-            }
 
-            for (let mounthgroup  of datalist){
-                mounthgroup.push(mounthgroup[1][0][6]);
-            }
-            console.log(datalist);
-            return datalist
-        }
-    },
-    mounted: function () {
+// ======================================================
 
-        fetch(requests_url).then(
-            data => data.json()
-    ).then(
-            data => {
-                let today = this.todaypost();
-                let Datetody = new Date(today);
-                for (let item = 0; item <data[1].length ;item++){
-                    let Datedate = new Date(data[1][item][1])
-                    if (Datedate>Datetody){
-                        data[1].splice(item --, 1);
-                        console.log('穿越了');
-                    }
-                }
-                this.datalist = data[1];
-                this.listlenth = data[1].length;
-                this.user_lenth = data[0].length;
-                this.datalist_slice = this.slice(data[1]);
-                this.last_update_time =this.timezoon();
-                this.loadmore_display = true;
-                let link_list=[];
-                for (let item of data[1]){
-                    if (item[1] === today ){
-                        this.today_post +=1;
-                    }
-                    link_list.push(item[3]);
-                }
-                let arr = this.unique(link_list);
-                this.unique_live_link = arr.length;
-                for (let item of data[0]){
-                    if (item[3] === 'true' ){
-                        this.error +=1;
-                    }
-                }
-            }
-        )
+// 打印友链基本信息
+function loadStatistical(sdata){
+// 友链页面的挂载容器
+var container = document.getElementById('fcircleContainer');
+// 基本信息的html结构
 
+var messageBoard =`
+<div id="fMessageBoard">
+  <div class="fUpdatedTime">
+    <span class="fLabel">最近更新时间：</span><span class="fMessage">${sdata.last_updated_time}</span>
+  </div>
+  <div class="fMessageItem">
+    <div class="fActiveFriend fItem">
+      <span class="fLabel">活跃友链数</span>
+      <span class="fMeasureBar"><span class="fMeasure" style="width:${(sdata.active_num/sdata.friends_num * 100).toFixed(2)}%; background: rgba(89, 230, 54,0.6);">${(sdata.active_num/sdata.friends_num * 100).toFixed(2)}%</span></span>
+      <span class="fMessage">${sdata.active_num}/${sdata.friends_num}</span>
+    </div>
+    <div class="fErrorSite fItem">
+      <span class="fLabel">失联友链数</span>
+      <span class="fMeasureBar"><span class="fMeasure" style="width:${(sdata.error_num/sdata.friends_num * 100).toFixed(2)}%; background: rgba(227, 23, 72, 0.6);">${(sdata.error_num/sdata.friends_num * 100).toFixed(2)}%</span></span>
+      <span class="fMessage">${sdata.error_num}/${sdata.friends_num}</span>
+    </div>
+    <div class="fArticleNum fItem">
+      <span class="fLabel">当前库存</span>
+      <span class="fMeasureBar"><span class="fMeasure" style="width:${(sdata.article_num/Math.ceil(sdata.article_num / 100)).toFixed(2)}%; background: rgba(29, 217, 211, 0.6);">${(sdata.article_num/Math.ceil(sdata.article_num / 100)).toFixed(2)}%</span></span>
+      <span class="fMessage">${sdata.article_num}/${Math.ceil(Number( sdata.article_num) / 100) * 100}</span>
+    </div>
+  </div>
+  <div id="switchRankBtn">
+    <span id="rankByCreated">created</span>
+    <span>
+    <input type="checkbox" id="switchRankMode" checked="true" onchange="checkRankMode()"/><label for="switchRankMode" id="switchRank">Toggle</label>
+    </span>
+    <span id="rankByUpdated">updated</span>
+  </div>
+</div>
+`;
+// 加载更多按钮
+var loadMoreBtn = `
+<div id="fcircleMoreBtn" onclick="loadMoreArticle()">
+  <i class="fas fa-angle-double-down"></i>
+</div>
+`
+// 原则上信息面板应该在最前面，所以用beforebegin表示从开始符前面插入
+if(container){
+  container.insertAdjacentHTML('beforebegin', messageBoard);
+  // 为了不影响文章加载，选择afterend表示从结束符后面插入
+  container.insertAdjacentHTML('afterend', loadMoreBtn);
+  }
+}
+
+// ======================================================
+// 打印友链内容
+function loadArticleItem(datalist,start,end){
+// 声明友链页面的挂载容器
+var container = document.getElementById('fcircleContainer');
+// 循环读取输出友链信息
+for (var i = start;i<end;i++){
+var item = datalist[i];
+var articleItem=`
+  <div class="fArticleItem">
+    <div class="fArticleAvatar">
+      <a class="fArticlelink fAvatar" target="_blank" rel="noopener nofollow" href="${item.link}">
+        <img src="${item.avatar}" alt="avatar">
+      </a>
+      <div class="fArticleAuthor">
+        ${item.author}
+      </div>
+    </div>
+    <div class="fArticleMessage">
+      <a class="fArticleTitle"  href="${item.link}" target="_blank" rel="noopener nofollow" data-title="${item.title}">
+        ${item.title}
+      </a>
+      <div class="fArticleTime">
+        <span class="fArticleCreated"><i class="far fa-calendar-alt">发表于</i>${item.created}</span>
+        <span class="fArticleUpdated"><i class="fas fa-history">更新于</i>${item.updated}</span>
+      </div>
+    </div>
+  </div>
+`;
+if(container){
+// 为了便于和后续拼接，选择从容器尾部插入
+container.insertAdjacentHTML('beforeend', articleItem);
     }
-})
+  }
+}
+
+
+// 初始化方法
+function initFriendCircle(){
+  // 获取友链挂载容器
+  var fcircleContainer = document.getElementById('fcircleContainer')
+  // 获取本地存储的友链基本信息
+  var statistical_data = JSON.parse(localStorage.getItem("statisticalList"));
+  // 从本地内存读取配置信息
+  var fdatalist = JSON.parse(localStorage.getItem("fdatalist"));
+  // 只有当容器、基本信息均存在时才执行初始化
+  if (fcircleContainer && statistical_data && fdatalist){
+    // 加载基本信息面板
+    loadStatistical(statistical_data);
+    // 获取切换排序按钮
+    var switchRankMode = document.getElementById("switchRankMode");
+    //根据当前选择的排序方案加载对应的排序内容
+    if(switchRankMode.checked){
+      // console.log("按更新时间排序");
+      var article_sortupdated = JSON.parse(localStorage.getItem("updatedList"));
+      loadArticleItem(article_sortupdated ,0,fdatalist.initnumber)
+    }else{
+      // console.log("按创建时间排序");
+      var article_sortcreated = JSON.parse(localStorage.getItem("createdList"));
+      loadArticleItem(article_sortcreated ,0,fdatalist.initnumber)
+    }
+  }
+}
+
+// 加载更多文章
+function loadMoreArticle(){
+  // 获取当前已加载的文章数
+  var currentArticle = document.getElementsByClassName('fArticleItem').length;
+  // 获取当前选择的排序方式
+  var switchRankMode = document.getElementById("switchRankMode");
+  // 从本地内存读取配置信息
+  var fdatalist = JSON.parse(localStorage.getItem("fdatalist"));
+  // 只有当两者均存在时才继续执行
+  if (switchRankMode && fdatalist){
+    if(switchRankMode.checked){
+      // console.log("按更新时间排序");
+      var article_sortupdated = JSON.parse(localStorage.getItem("updatedList"));
+      // 从当前文章的下一篇开始，加载下一阶程篇数
+      loadArticleItem(article_sortupdated,currentArticle,currentArticle + fdatalist.stepnumber)
+    }else{
+      // console.log("按创建时间排序");
+      var article_sortcreated = JSON.parse(localStorage.getItem("createdList"));
+      // 从当前文章的下一篇开始，加载下一阶程篇数
+      loadArticleItem(article_sortcreated,currentArticle,currentArticle + fdatalist.stepnumber)
+    }
+  }
+  // 向上滚动一篇文章的距离
+  window.scrollBy(0,180)
+}
+
+//切换按钮
+function checkRankMode(){
+  // 首先清空现有的文章内容
+  document.getElementById('fcircleContainer').innerHTML=''
+  // 获取当前选择的排序方式
+  var switchRankMode = document.getElementById("switchRankMode");
+  // 从本地内存读取配置信息
+  var fdatalist = JSON.parse(localStorage.getItem("fdatalist"));
+  // 只有当两者均存在时才继续执行
+  if (switchRankMode && fdatalist){
+    //按更新时间排序
+    if(switchRankMode.checked){
+      // console.log("按更新时间排序");
+      var article_sortupdated = JSON.parse(localStorage.getItem("updatedList"));
+      //加载配置项中指定的初始化篇数
+      loadArticleItem(article_sortupdated ,0,fdatalist.initnumber)
+    }else{
+      // console.log("按创建时间排序");
+      var article_sortcreated = JSON.parse(localStorage.getItem("createdList"));
+      //加载配置项中指定的初始化篇数
+      loadArticleItem(article_sortcreated ,0,fdatalist.initnumber)
+    }
+  }
+}
+//执行初始化方法
+initFriendCircle()
